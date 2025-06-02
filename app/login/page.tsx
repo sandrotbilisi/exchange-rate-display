6c46b08cc5ae815018d72b6eb885ced6ddc4c2dc
sandrotbilisi <atsetsoffc@gmail.com>
2025-06-02 17:06:07 +0200

    chadcn working + basic login layout

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import  { FormComponent } from './form';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <FormComponent />
+      {/* <Button>Login</Button> */}
+    </div>
+  );
+};
+
+export default LoginPage;
